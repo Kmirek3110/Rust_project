90fc4956ca3f8e238b629bd5caffe093450a70fa
Kmirek3110 <chemionpl@gmail.com>
2020-01-03 19:53:52 +0100

    better

--- projekt/src/reg.rs
+++ projekt/src/reg.rs
@@ -7,47 +7,47 @@ pub fn check_reg_lin(cos :&Vec<Record>, fieldname: String)->(f64,f64){
     let res : (f64,f64);
     if fieldname == "fixed_acid".to_string()
     {
-         res = fixed_acid_reg(cos);
+         res = linear_regression((fixed_acid_reg(cos))).unwrap();
     }
     else if fieldname == "volatile_acid".to_string()
     {
-        res = volatile_acid_reg(cos);
+        res = linear_regression((volatile_acid_reg(cos))).unwrap();
     }
     else if fieldname == "citric_acid".to_string()
     {
-        res = citric_acid_reg(cos);
+        res = linear_regression((citric_acid_reg(cos))).unwrap();
     }
     else if fieldname == "residual_sugar".to_string()
     {
-        res = residual_sugar_reg(cos);
+        res = linear_regression((residual_sugar_reg(cos))).unwrap();
     }
     else if fieldname == "chlorides".to_string()
     {
-        res = chlorides_reg(cos);
+        res = linear_regression((chlorides_reg(cos))).unwrap();
     }
     else if fieldname == "free_sulfur_diox".to_string()
     {
-        res = free_sulfur_diox_reg(cos);
+        res = linear_regression((free_sulfur_diox_reg(cos))).unwrap();
     }
     else if fieldname == "total_sulfur_diox".to_string()
     {
-        res = total_sulfur_diox_reg(cos);
+        res = linear_regression((total_sulfur_diox_reg(cos))).unwrap();
     }
     else if fieldname == "density".to_string()
     {
-        res = density_reg(cos);
+        res = linear_regression((density_reg(cos))).unwrap();
     }
     else if fieldname == "ph".to_string()
     {
-        res = ph_reg(cos);
+        res = linear_regression((ph_reg(cos))).unwrap();
     }
     else if fieldname == "sulphates".to_string()
     {
-        res = sulphates_reg(cos);
+        res = linear_regression((sulphates_reg(cos))).unwrap();
     }
     else if fieldname == "alcohol".to_string()
     {
-        res = alcohol_reg(cos);
+        res = linear_regression((alcohol_reg(cos))).unwrap();
     }
     else 
     {
@@ -57,102 +57,102 @@ pub fn check_reg_lin(cos :&Vec<Record>, fieldname: String)->(f64,f64){
     res
     
 }
-pub fn fixed_acid_reg(cos :&Vec<Record>)-> (f64,f64){
+pub fn fixed_acid_reg(cos :&Vec<Record>)-> (Vec<f64>,Vec<f64>){
     let mut reg_vector :Vec<f64> = Vec::new();
     let mut reg_target :Vec<f64> = Vec::new();
     for i in cos {
         reg_vector.push(i.fixed_acid as f64);
         reg_target.push(i.quality as f64);
     }
-    linear_regression(&reg_vector,&reg_target).unwrap()
+    (reg_vector,reg_target)
 }
-pub fn volatile_acid_reg(cos :&Vec<Record>)-> (f64,f64){
+pub fn volatile_acid_reg(cos :&Vec<Record>)-> (Vec<f64>,Vec<f64>){
     let mut reg_vector :Vec<f64> = Vec::new();
     let mut reg_target :Vec<f64> = Vec::new();
     for i in cos {
         reg_vector.push(i.volatile_acid as f64);
         reg_target.push(i.quality as f64);
     }
-    linear_regression(&reg_vector,&reg_target).unwrap()
+    (reg_vector,reg_target)
 }
-pub fn citric_acid_reg(cos :&Vec<Record>)-> (f64,f64){
+pub fn citric_acid_reg(cos :&Vec<Record>)-> (Vec<f64>,Vec<f64>){
     let mut reg_vector :Vec<f64> = Vec::new();
     let mut reg_target :Vec<f64> = Vec::new();
     for i in cos {
         reg_vector.push(i.citric_acid as f64);
         reg_target.push(i.quality as f64);
     }
-    linear_regression(&reg_vector,&reg_target).unwrap()
+    (reg_vector,reg_target)
 }
-pub fn residual_sugar_reg(cos :&Vec<Record>)-> (f64,f64){
+pub fn residual_sugar_reg(cos :&Vec<Record>)-> (Vec<f64>,Vec<f64>){
     let mut reg_vector :Vec<f64> = Vec::new();
     let mut reg_target :Vec<f64> = Vec::new();
     for i in cos {
         reg_vector.push(i.residual_sugar as f64);
         reg_target.push(i.quality as f64);
     }
-    linear_regression(&reg_vector,&reg_target).unwrap()
+    (reg_vector,reg_target)
 }
-pub fn chlorides_reg(cos :&Vec<Record>)-> (f64,f64){
+pub fn chlorides_reg(cos :&Vec<Record>)-> (Vec<f64>,Vec<f64>){
     let mut reg_vector :Vec<f64> = Vec::new();
     let mut reg_target :Vec<f64> = Vec::new();
     for i in cos {
         reg_vector.push(i.chlorides as f64);
         reg_target.push(i.quality as f64);
     }
-    linear_regression(&reg_vector,&reg_target).unwrap()
+    (reg_vector,reg_target)
 }
-pub fn free_sulfur_diox_reg(cos :&Vec<Record>)-> (f64,f64){
+pub fn free_sulfur_diox_reg(cos :&Vec<Record>)-> (Vec<f64>,Vec<f64>){
     let mut reg_vector :Vec<f64> = Vec::new();
     let mut reg_target :Vec<f64> = Vec::new();
     for i in cos {
         reg_vector.push(i.free_sulfur_diox as f64);
         reg_target.push(i.quality as f64);
     }
-    linear_regression(&reg_vector,&reg_target).unwrap()
+    (reg_vector,reg_target)
 }
-pub fn total_sulfur_diox_reg(cos :&Vec<Record>)-> (f64,f64){
+pub fn total_sulfur_diox_reg(cos :&Vec<Record>)-> (Vec<f64>,Vec<f64>){
     let mut reg_vector :Vec<f64> = Vec::new();
     let mut reg_target :Vec<f64> = Vec::new();
     for i in cos {
         reg_vector.push(i.total_sulfur_diox as f64);
         reg_target.push(i.quality as f64);
     }
-    linear_regression(&reg_vector,&reg_target).unwrap()
+    (reg_vector,reg_target)
 }
-pub fn density_reg(cos :&Vec<Record>)-> (f64,f64){
+pub fn density_reg(cos :&Vec<Record>)-> (Vec<f64>,Vec<f64>){
     let mut reg_vector :Vec<f64> = Vec::new();
     let mut reg_target :Vec<f64> = Vec::new();
     for i in cos {
         reg_vector.push(i.density as f64);
         reg_target.push(i.quality as f64);
     }
-    linear_regression(&reg_vector,&reg_target).unwrap()
+    (reg_vector,reg_target)
 }
-pub fn ph_reg(cos :&Vec<Record>)-> (f64,f64){
+pub fn ph_reg(cos :&Vec<Record>)-> (Vec<f64>,Vec<f64>){
     let mut reg_vector :Vec<f64> = Vec::new();
     let mut reg_target :Vec<f64> = Vec::new();
     for i in cos {
         reg_vector.push(i.ph as f64);
         reg_target.push(i.quality as f64);
     }
-    linear_regression(&reg_vector,&reg_target).unwrap()
+    (reg_vector,reg_target)
 }
-pub fn sulphates_reg(cos :&Vec<Record>)-> (f64,f64){
+pub fn sulphates_reg(cos :&Vec<Record>)-> (Vec<f64>,Vec<f64>){
     let mut reg_vector :Vec<f64> = Vec::new();
     let mut reg_target :Vec<f64> = Vec::new();
     for i in cos {
         reg_vector.push(i.sulphates as f64);
         reg_target.push(i.quality as f64);
     }
-    linear_regression(&reg_vector,&reg_target).unwrap()
+    (reg_vector,reg_target)
 }
-pub fn alcohol_reg(cos :&Vec<Record>)-> (f64,f64){
+pub fn alcohol_reg(cos :&Vec<Record>)-> (Vec<f64>,Vec<f64>){
     let mut reg_vector :Vec<f64> = Vec::new();
     let mut reg_target :Vec<f64> = Vec::new();
     for i in cos {
         reg_vector.push(i.alcohol as f64);
         reg_target.push(i.quality as f64);
     }
-    linear_regression(&reg_vector,&reg_target).unwrap()
+    (reg_vector,reg_target)
 }
